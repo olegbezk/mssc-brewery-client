38bc42a15040afe5d864518a134b7575d1e8efb4
Oleg Bezkorovaynyi <oleg.bezkorovaynyi@globallogic.com>
2019-12-09 13:47:48 +0200

    handle post

--- src/test/java/guru/springframework/msscbreweryclient/web/client/BreweryClientTest.java
+++ src/test/java/guru/springframework/msscbreweryclient/web/client/BreweryClientTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.net.URI;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -20,5 +21,16 @@ class BreweryClientTest {
         final BeerDto beerById = breweryClient.getBeerById(UUID.randomUUID());
 
         assertNotNull(beerById);
+    }
+
+    @Test
+    void testSaveBeer() {
+        final BeerDto newBeer = BeerDto.builder().beerName("New Beer").build();
+
+        final URI uri = breweryClient.saveBeer(newBeer);
+
+        assertNotNull(uri);
+
+        System.out.println(uri);
     }
 }
